ade1eb030db688efbc3d88074f54830f5ee4d7e6
Manuel Garcia F <mgarciafrigo@gmail.com>
2021-05-25 11:47:35 -0300

    initial cleanup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsa error nostrum aspernatur
+        aperiam ratione fuga. Cumque perspiciatis, omnis illum labore recusandae accusamus autem
+        dolores, tempore expedita, exercitationem ducimus officia!
+      </p>
+    </div>
+  );
+}
+
+export default App;
